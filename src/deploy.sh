31b378ef8fee5b387e92f3a0e6af188272a82297
Ali Farahani <45420649+a-m-farahani@users.noreply.github.com>
2023-11-06 19:11:29 +0330

    Update deploy.sh

--- src/deploy.sh
+++ src/deploy.sh
@@ -2,9 +2,4 @@
 
 echo "pull confluent kafka image from docker hub..."
 docker pull confluentinc/cp-kafka:7.5.0
-
-echo "pull confluent kafka-connect image from docker hub..."
-docker pull confluentinc/cp-kafka-connect:7.5.0
-
 docker compose -f kafka-compose.yaml up -d
-
